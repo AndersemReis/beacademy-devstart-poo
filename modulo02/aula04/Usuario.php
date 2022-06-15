9689f8cefb372826840b61377b835e44827f9688
AndersemReis <andersem.Reis@gmail.com>
2022-06-14 20:25:46 -0400

    Classes abstratas

--- modulo02/aula04/Usuario.php
+++ modulo02/aula04/Usuario.php
@@ -2,7 +2,8 @@
 
 declare (strict_types=1);
 
-class Usuario{
+abstract class Usuario 
+{
     private string $nome;
     private string $email;
     private string $senha;
--- modulo02/aula04/index.php
+++ modulo02/aula04/index.php
@@ -2,6 +2,7 @@
 
 include 'Usuario.php';
 include 'Cliente.php';
+//include 'ClienteVip.php';
 include 'Gestor.php';
 include 'GestorGeral.php';
 
@@ -15,6 +16,9 @@ $g1->setNome('Zezim');
 $gg1 = new GestorGeral('pedim@gmail.com','123456789',9000);
 $gg1->setNome('pedrim');
 
+//$clienteVip = new ClientVip('clientevip@gmail.com', '123456');
+
 var_dump($c1);
 var_dump($g1);
 var_dump($gg1);
+//var_dump($clienteVip);
