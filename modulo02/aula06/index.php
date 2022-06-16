78d856c623c7f0f025ea756991e20b5b2960a9c0
AndersemReis <andersem.Reis@gmail.com>
2022-06-16 15:58:59 -0400

    usando autoload

--- modulo02/aula06/index.php
+++ modulo02/aula06/index.php
@@ -1,8 +1,15 @@
 <?php
 
-include 'Classes/Usuario.php';
-include 'Classes/Config/Usuario.php';
-include 'Classes/Config/Database/MySql.php';
+include 'vendor/autoload.php';
 
+use Classes\Config\Usuario;
+use Classes\Categoria;
 
+//$us1 = new Classes\Usuario();
+//$us2 = new Classes\Config\Usuario();
+$us2 = new Usuario();
+$c1 = new Categoria();
+//var_dump($us1);
+var_dump($us2);
+var_dump($c1);
 echo 'funcionou';
