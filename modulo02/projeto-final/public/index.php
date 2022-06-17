ef94bced8ce1753465efba7fc7285bae92d82db7
AndersemReis <andersem.Reis@gmail.com>
2022-06-17 14:53:52 -0400

    abstract controller projeto final

--- modulo02/projeto-final/public/index.php
+++ modulo02/projeto-final/public/index.php
@@ -2,12 +2,12 @@
 include 'modulo02\projeto-final\vendor\autoload.php';
 use App\Controller\IndexController;
 use App\Controller\ProductController;
-
+use App\Controller\CategoryController;
 $p = new ProductController();
-
+$ca = new CategoryController();
 $c = new IndexController();
 
 $c -> loginAction();
 $p->listAction();
-
+$ca->listAction();
 $p->editAction();
---
+++ modulo02/projeto-final/src/View/category/add.php
@@ -0,0 +1,1 @@
+<h1>adicionando categpria<h1>
