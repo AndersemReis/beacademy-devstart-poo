8fb5fec3e37841fd5e4f5f982b6546cf93836dd6
AndersemReis <andersem.Reis@gmail.com>
2022-06-17 15:27:05 -0400

    Final de POO projeto final

--- modulo02/projeto-final/public/index.php
+++ modulo02/projeto-final/public/index.php
@@ -3,11 +3,30 @@ include 'modulo02\projeto-final\vendor\autoload.php';
 use App\Controller\IndexController;
 use App\Controller\ProductController;
 use App\Controller\CategoryController;
-$p = new ProductController();
-$ca = new CategoryController();
-$c = new IndexController();
+use App\Controller\ErrorController;
 
-$c -> loginAction();
-$p->listAction();
-$ca->listAction();
-$p->editAction();
+$url = explode('?',$_SERVER['REQUEST_URI'])[0];
+
+function createRoute(string $controllerName, string $methodName){
+    return [
+        'controller' => $controllerName,
+        'method' => $methodName,
+    ];
+}
+
+$routes = [
+    '/' => createRoute(IndexController::class, 'indexAction'),
+    '/produtos' => createRoute(ProductController::class, 'listAction'),
+    '/produtos/novo' => createRoute(ProductController::class, 'addAction')
+];
+
+if(false === isset($routes[$url])) {
+    (new ErrorController()) -> notFoundAction();
+    exit;
+
+}
+
+$controllerName = $routes[$url]['controller'];
+$methodName = $routes[$url]['method'];
+
+(new $controllerName()) -> $methodName();
